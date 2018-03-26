069f875f6fd98696b020cd78f61eb8209ef037ce
velociwabbit <jimreillyemail@gmail.com>
2018-03-26 12:30:05 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var cfg=    
 [ 
-   { SPORT :   '3003' ,   SNPRT :   '3330' ,   DBPORT :   '3306' ,   DUSER :   'root' ,   DPASS :   'Passw0rd'  ,   DBHOST :   'ctest.cocxrzkw7a6m.us-west-2.rds.amazonaws.com'     , DBASE    : 'crypton'     , CLIENT    : true       , SERVER : true }
-,  { SPORT :   '3003' ,   SNPRT :   '3330' ,   DBPORT :   '3306' ,   DUSER :   'root' ,   DPASS :   'pass'       ,   DBHOST :   'localhost'                                         ,  URIHOST : 'localhost'   , EXTERNURL : 'localhost', DBASE : 'crypton' , CLIENT : true, SERVER : true }
+   { SPORT :   '3003' ,   SNPRT :   '3330' ,   DBPORT :   '3306' ,   DUSER :   'root' ,   DPASS :   'pass'       ,   DBHOST :   'localhost'     , DBASE    : 'gdprdd'     , CLIENT    : true       , SERVER : true }
+,  { SPORT :   '3003' ,   SNPRT :   '3330' ,   DBPORT :   '3306' ,   DUSER :   'root' ,   DPASS :   'pass'       ,   DBHOST :   'localhost'    ,  URIHOST : 'localhost'   , EXTERNURL : 'localhost', DBASE : 'gdprdd' , CLIENT : true, SERVER : true }
  ]
  
 var config = cfg[0]
